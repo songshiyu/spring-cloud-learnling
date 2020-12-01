0ef95461ef42656f92f33c47c17a51d264e2de58
lxk <songshiyu_s@163.com>
2020-12-01 22:28:29 +0800

    HystrixCommand 服务的多次降级

--- hystrix/hystrix-fallback/src/main/java/com/lxk/springcloud/hystrix/Fallback.java
+++ hystrix/hystrix-fallback/src/main/java/com/lxk/springcloud/hystrix/Fallback.java
@@ -2,6 +2,7 @@ package com.lxk.springcloud.hystrix;
 
 import com.lxk.springcloud.pojo.Friend;
 import com.lxk.springcloud.service.MyService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +15,27 @@ import org.springframework.stereotype.Component;
 public class Fallback implements MyService {
 
     @Override
+    @HystrixCommand(fallbackMethod = "fallback1")
     public String error() {
         log.info("Fallback: I'm not a black sheep any more");
-        return "first fallback";
+        throw new RuntimeException("falback");
+        //return "first fallback";
+    }
+
+    @HystrixCommand(fallbackMethod = "fallback2")
+    public String fallback1() {
+        log.info("fallback again");
+        throw new RuntimeException("fallback");
+    }
+
+    @HystrixCommand(fallbackMethod = "fallback3")
+    public String fallback2() {
+        log.info("fallback again and again");
+        throw new RuntimeException("fallback");
+    }
+
+    public String fallback3() {
+        return "success";
     }
 
     @Override
