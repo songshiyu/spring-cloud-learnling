95affbda16e991092089db301556be6211e19baa
lxk <songshiyu_s@163.com>
2020-12-01 22:09:53 +0800

    添加RequestCache，请求缓存

--- hystrix/hystrix-fallback/src/main/java/com/lxk/springcloud/controller/Controller.java
+++ hystrix/hystrix-fallback/src/main/java/com/lxk/springcloud/controller/Controller.java
@@ -1,6 +1,10 @@
 package com.lxk.springcloud.controller;
 
+import com.lxk.springcloud.hystrix.RequestCacheService;
+import com.lxk.springcloud.pojo.Friend;
 import com.lxk.springcloud.service.MyService;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
+import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +21,9 @@ public class Controller {
     @Autowired
     private MyService myService;
 
+    @Autowired
+    private RequestCacheService requestCacheService;
+
     @GetMapping("/fallback")
     public String fallback() {
         return myService.error();
@@ -25,5 +32,16 @@ public class Controller {
     @GetMapping("/timeout")
     public String timeout(Integer timeout) {
         return myService.retry(timeout);
+    }
+
+    @GetMapping("/cache")
+    public Friend cache(String name) {
+
+        @Cleanup HystrixRequestContext hystrixRequestContext =
+                HystrixRequestContext.initializeContext();
+
+        Friend friend = requestCacheService.requestCache(name);
+        friend = requestCacheService.requestCache(name);
+        return friend;
     }
 }
