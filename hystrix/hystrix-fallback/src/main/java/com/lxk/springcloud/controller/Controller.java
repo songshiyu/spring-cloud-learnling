4c760de79a3a4d61e8150d648e522a10a8d285e1
lxk <songshiyu_s@163.com>
2020-12-01 22:35:44 +0800

    在具体的方法上指定超时时间及超时后的回调函数

--- hystrix/hystrix-fallback/src/main/java/com/lxk/springcloud/controller/Controller.java
+++ hystrix/hystrix-fallback/src/main/java/com/lxk/springcloud/controller/Controller.java
@@ -3,6 +3,8 @@ package com.lxk.springcloud.controller;
 import com.lxk.springcloud.hystrix.RequestCacheService;
 import com.lxk.springcloud.pojo.Friend;
 import com.lxk.springcloud.service.MyService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
@@ -29,10 +31,24 @@ public class Controller {
         return myService.error();
     }
 
+    /**
+     * 在具体的方法上指定超时时间及超时后的回调函数
+     * */
     @GetMapping("/timeout")
+    @HystrixCommand(
+            fallbackMethod = "timeout2",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+            }
+    )
     public String timeout(Integer timeout) {
         return myService.retry(timeout);
     }
+
+    public String timeout2(Integer timeout) {
+        return "success";
+    }
+
 
     @GetMapping("/cache")
     public Friend cache(String name) {
