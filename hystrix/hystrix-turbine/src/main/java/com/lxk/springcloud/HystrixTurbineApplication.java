e716096e6fa1fd3c8a3de8495c4e95a965c4d63b
lxk <songshiyu_s@163.com>
2020-12-04 22:04:37 +0800

    添加turbine，收集服务接口信息

---
+++ hystrix/hystrix-turbine/src/main/java/com/lxk/springcloud/HystrixTurbineApplication.java
@@ -0,0 +1,27 @@
+package com.lxk.springcloud;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author songshiyu
+ * @date 2020/12/4 21:53
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableTurbine
+public class HystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixTurbineApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
