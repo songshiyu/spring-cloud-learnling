00770913af7731d540ad528859ec4f94fea5bafb
lxk <songshiyu_s@163.com>
2020-12-04 22:27:59 +0800

    Hystirx针对接口健康及降级等情况的监控大盘

---
+++ hystrix/hystrix-dashboard/src/main/java/com/lxk/springcloud/HystrixDashboardApplication.java
@@ -0,0 +1,21 @@
+package com.lxk.springcloud;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author songshiyu
+ * @date 2020/12/4 22:25
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixDashboardApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
